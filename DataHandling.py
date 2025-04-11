4bb4b5e2682ce6109273d2a272fb2a79cc7dfd01
Insanebtblocks <154077791+Insanebtblocks@users.noreply.github.com>
2025-04-11 12:43:45 +0200

    Datahandling v1
    Datahandling for ai training data

---
+++ DataHandling.py
@@ -0,0 +1,78 @@
+from gensim.models import Word2Vec
+import numpy as np
+from nltk.tokenize import word_tokenize
+
+temp_sentences = ["pad hey how are you im good and you eos","pad pad whats your name my name is loyd eos","what colour is the sky the sky is blue eos"]
+Training_sentences = ["hey how are you ,im good and you eos","whats your name ,my name is loyd eos","what colour is the sky ,the sky is blue eos"]
+
+sentences = [word_tokenize(sentence.lower()) for sentence in temp_sentences] #To kenizing the sentence
+print(sentences)
+
+word2vec_model = Word2Vec(sentences=sentences,vector_size=100,workers=4,window=5,min_count=1)
+
+
+def word_to_vector(word):
+    try:
+        vector = word2vec_model.wv(word)
+    except:
+        vector = np.zeros(100)
+    return vector
+
+def vector_to_word(vector):
+    word = word2vec_model.wv.similar_by_vector(vector,topn=1)[0][0]
+    return word
+
+def Training_Data(window_size):
+    x_training = []
+    y_training = []
+    split_training = []
+    for index in range(len(Training_sentences)):
+        split_sentence = Training_sentences[index].split(",")
+        x = split_sentence[0].split()
+        y= split_sentence[1].split()
+        if len(x) < window_size:
+            while len(x) < window_size:
+                x.insert(0,"pad")
+            x_training.append(x.copy())
+            y_training.append(y[0])
+            for output_index in range(1,len(y)):
+                x.pop(0)
+                x.append(y[output_index-1])
+                if output_index < len(y):
+                    x_training.append(x.copy())
+                    y_training.append(y[output_index])
+            
+        elif len(x) > window_size:
+            pass#Sort out later
+        else:
+            x_training.append(x.copy())
+            y_training.append(y[0])
+            for output_index in range(1,len(y)):
+                x.pop(0)
+                x.append(y[output_index-1])
+                if output_index < len(y):
+                    x_training.append(x.copy())
+                    y_training.append(y[output_index])
+
+    return x_training,y_training
+
+X,Y = Training_Data(5)
+
+def training_to_vector(Input,outputs):
+    x_vector_training = []
+    y_vector_training = []
+    for index in range(len(Input)):
+        vector = []
+        for input_index in range(len(Input[index])):
+            try:
+                vector.append(word2vec_model.wv[Input[index][input_index]])
+            except:
+                vector.append(np.zeros(100))
+        
+        x_vector_training.append(np.array(vector).flatten())
+        y_vector_training.append(word2vec_model.wv[outputs[index]])
+    return x_vector_training,y_vector_training
+
+x_vec,y_vec = training_to_vector(X,Y)
+print(x_vec,y_vec)
+
--- tensorai.py
+++ tensorai.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import DataHandling as DH
 
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(100,activation="leaky_relu",input_shape=(500,)),
@@ -8,8 +9,7 @@ model = tf.keras.Sequential([
 
 model.compile(optimizer='adam',loss=tf.keras.losses.CosineSimilarity(axis=1),metrics=['accuracy'])
 
-x = np.random.uniform(-1,1,(500,))
-y= np.random.uniform(-1,1,(100,))
-print(np.array(x).shape)
-prediction = model.predict(np.array([x]))
-model.fit(np.array([x]),np.array([y]),epochs=10,verbose=1)
+x,y = DH.Training_Data(5)
+X,Y = DH.training_to_vector(x,y)
+
+model.fit(np.array(X),np.array(Y),epochs=1000,verbose=1)
