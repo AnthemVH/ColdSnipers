8ddc989fcbbf8f16fc940fa3925a3332faa22dce
Insanebtblocks <154077791+Insanebtblocks@users.noreply.github.com>
2025-04-12 04:19:48 +0200

    yes
    yes

--- Frontend/Loyd.py
+++ Frontend/Loyd.py
@@ -125,7 +125,7 @@ def Answer_prompt(prompt):
 
 x, y = DH.Training_Data(5)
 X, Y = DH.training_to_vector(x, y)
-train(X, Y, epochs=10000, lr=0.005)
-save_model()
+#train(X, Y, epochs=10000, lr=0.005)
+s#ave_model()
 
 print("\nResponse:", Answer_prompt("when is my exam starting"))
